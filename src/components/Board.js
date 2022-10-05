5297bff98d94ad6aa90b939db233371bd60f9316
xAlice2 <helena.wu4@gmail.com>
2022-10-04 19:57:07 -0700

    changed board component to function

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,38 +2,75 @@ import React from 'react';  // because this extends react.component
 import Square from './Square';    // because it contains the component Square
 
 
- class Board extends React.Component {
-    renderSquare(i) {
-      return (
-        <Square
-          value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}
-        />
-      );
+/**
+ * changing from class to function
+ */
+function Board(props) {
+
+    const renderSquare = (i) => {
+        return (
+            <Square
+                value={props.squares[i]}    //this.props only applies to class component, here we replace with props because its passed through the function
+                onClick={() => props.onClick(i)}
+            />
+        );
     }
-  
-    render() {
-      return (
+
+    return (
         <div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
           </div>
         </div>
       );
-    }
-  }
+}
+
+// ===================
+
+//  class Board extends React.Component {
+//     renderSquare(i) {
+//       return (
+//         <Square
+//           value={this.props.squares[i]}
+//           onClick={() => this.props.onClick(i)}
+//         />
+//       );
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           <div className="board-row">
+//             {this.renderSquare(0)}
+//             {this.renderSquare(1)}
+//             {this.renderSquare(2)}
+//           </div>
+//           <div className="board-row">
+//             {this.renderSquare(3)}
+//             {this.renderSquare(4)}
+//             {this.renderSquare(5)}
+//           </div>
+//           <div className="board-row">
+//             {this.renderSquare(6)}
+//             {this.renderSquare(7)}
+//             {this.renderSquare(8)}
+//           </div>
+//         </div>
+//       );
+//     }
+//   }
 
 
 export default Board;
